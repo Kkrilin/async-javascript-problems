652bbf8085f8934db82373e59fdc8458ed9c0837
kundan singh patel <kundansingh066@gmail.com>
2023-09-08 16:32:54 +0530

    file for testing problem 1

---
+++ test/testProblem1.js
@@ -0,0 +1,10 @@
+const myModule = require('../problem1');
+const callBackFs = myModule.callBackFs;
+const usingPromise = myModule.usingPromise;
+const usingAsyncAwait = myModule.usingAsyncAwait;
+
+callBackFs('callback');
+
+// usingPromise('promiseChain');
+
+// usingAsyncAwait('asyncAwait');
